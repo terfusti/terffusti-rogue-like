caee455cfbac78618bb2fb1a90d707b4a5cf030f
terfusti <thor.lawing@gmail.com>
2021-11-20 01:18:40 -0500

    fixed license and update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,44 @@
 
 #include <cstdlib>
 
-int main(int argc, char** argv) {
-  auto params = TCOD_ContextParams{};
-  params.tcod_version = TCOD_COMPILEDVERSION;
-  params.argc = argc;
-  params.argv = argv;
-  params.vsync = 1;
-  params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
-  params.window_title = "Libtcod Template Project";
+int main(int argc, char** argv)
+{
+    auto params = TCOD_ContextParams{};
+    params.tcod_version = TCOD_COMPILEDVERSION;
+    params.argc = argc;
+    params.argv = argv;
+    params.vsync = 1;
+    params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
+    params.window_title = "Terfusti Rogue Like";
 
-  auto console = tcod::Console{80, 25};
-  params.console = console.get();
+    auto console = tcod::Console{ 80, 85 };
+    params.console = console.get();
 
-  auto context = tcod::new_context(params);
+    auto context = tcod::new_context(params);
 
-  // Game loop.
-  while (true) {
-    // Rendering.
-    console.clear();
-    tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
-    context->present(console);
+    while (true)
+    {
+        
+        console.clear();
+        tcod::print(console, { 0, 0 }, "Thor says hello!", TCOD_red, std::nullopt);
+        context->present(console);
 
-    // Handle input.
-    SDL_Event event;
-    SDL_WaitEvent(nullptr);
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          std::exit(EXIT_SUCCESS);
-          break;
-      }
+        SDL_Event event{};
+        SDL_WaitEvent(nullptr);
+        while (SDL_PollEvent)
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                std::exit(EXIT_SUCCESS);
+                break;
+            
+            default:
+                break;
+            }
+        }
+        
     }
-  }
-  return 0;
+    
+    return 0;
 }
